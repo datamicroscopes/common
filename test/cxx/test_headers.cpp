b9bfb0af269b2adfe2893825a52824a83b3cc620
Stephen Tu <stephen@qadium.com>
2014-08-08 16:16:39 -0700

    put the operator overloads in the right namespace

--- test/cxx/test_headers.cpp
+++ test/cxx/test_headers.cpp
@@ -1,6 +1,9 @@
 #include <microscopes/common/static_vector.hpp>
 #include <vector>
 #include <map>
+#include <iostream>
+
+using namespace std;
 
 int
 main(void)
@@ -9,9 +12,13 @@ main(void)
   microscopes::common::static_vector<size_t, 30> s2({1, 2});
   std::vector<unsigned> test3({1, 2, 3});
   microscopes::common::static_vector<unsigned, 3> s3(
-      test3.begin(), test3.end());
+      test3.begin(), test3.end()), s4;
 
-  std::map<microscopes::common::static_vector<int, 10>, int> m;
+  cout << (s3 < s4) << endl;
+
+  map<microscopes::common::static_vector<size_t, 30>, int> m;
+  m[s2] = 30;
+  cout << m.size() << endl;
 
   return 0;
 }
