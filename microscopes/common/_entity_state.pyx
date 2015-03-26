c33c8832829c819d7f78e6e1f3494c624a90a6a0
Tim Hopper <tdhopper@gmail.com>
2015-03-26 12:36:38 -0400

    Remove fixed references from _entity_state.pyx

--- microscopes/common/_entity_state.pyx
+++ microscopes/common/_entity_state.pyx
@@ -4,17 +4,13 @@
 from microscopes.models import model_descriptor
 import numpy as np
 
-cdef class fixed_entity_based_state_object:
+cdef class entity_based_state_object():
     def __init__(self, models):
         for m in models:
             if not isinstance(m, model_descriptor):
                 raise ValueError(
                     "expecting model_descriptor, got {}".format(repr(m)))
         self._models = list(models)
-
-    cdef void set_fixed(
-            self, const shared_ptr[c_fixed_entity_based_state_object] &o):
-        self._thisptr = o
 
     # expose enough of the API here
 
@@ -40,31 +36,6 @@ cdef class fixed_entity_based_state_object:
         return (
             [int(x) for x in ret.first],
             np.array([x for x in ret.second], dtype=np.float)
-        )
-
-cdef class entity_based_state_object(fixed_entity_based_state_object):
-
-    cdef void set_fixed(
-            self, const shared_ptr[c_fixed_entity_based_state_object] &o):
-        # XXX: hacky
-        raise Exception("no evidence of derived class")
-
-    cdef void set_entity(
-            self, const shared_ptr[c_entity_based_state_object] & o):
-        # Cython's type system is too weak to allow us to express this without
-        # the explicit pointer cast (in C++ this would be completely
-        # un-necessary)
-        self._thisptr = (
-            static_pointer_cast[
-                c_fixed_entity_based_state_object,
-                c_entity_based_state_object](o)
-        )
-
-    cdef shared_ptr[c_entity_based_state_object] px(self):
-        return (
-            static_pointer_cast[
-                c_entity_based_state_object,
-                c_fixed_entity_based_state_object](self._thisptr)
         )
 
     cdef c_entity_based_state_object * raw_px(self):
